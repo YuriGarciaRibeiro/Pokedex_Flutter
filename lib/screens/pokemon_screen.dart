0f83d46c84994f4ff902574cf99cf614a6c1f2b7
Yuri Garcia Ribeiro <81641949+YuriGarciaRibeiro@users.noreply.github.com>
2022-05-06 16:30:18 -0300

    Update pokemon_screen.dart

--- lib/screens/pokemon_screen.dart
+++ lib/screens/pokemon_screen.dart
@@ -4,12 +4,26 @@ import '../models/pokemon_model.dart';
 class PokemonScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Pokemon selectPokemon =
-        ModalRoute.of(context).settings.arguments;
+    final Pokemon selectPokemon = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
       appBar: AppBar(
         title: Text(selectPokemon.name),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              selectPokemon.name,
+              style: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'RobotoCondensed'),
+            ),
+            Text('teste')
+          ],
+        ),
       ),
     );
   }
